3f13bbe659fd313056dddfd0ef472599d47d5ed8
Timur Duehr <tduehr@gmail.com>
2018-08-13 22:50:26 -0500

    rest

--- support/cas-server-support-audit-rest/src/test/java/org/apereo/cas/audit/RestAuditTrailManagerTests.java
+++ support/cas-server-support-audit-rest/src/test/java/org/apereo/cas/audit/RestAuditTrailManagerTests.java
@@ -1,28 +1,33 @@
 package org.apereo.cas.audit;
 
+import org.apereo.cas.audit.spi.BaseAuditConfigurationTests;
 import org.apereo.cas.config.CasCoreUtilSerializationConfiguration;
-import org.apereo.cas.configuration.model.core.audit.AuditRestProperties;
+import org.apereo.cas.config.CasSupportRestAuditConfiguration;
 import org.apereo.cas.util.CollectionUtils;
 import org.apereo.cas.util.MockWebServer;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Getter;
 import lombok.val;
 import org.apereo.inspektr.audit.AuditActionContext;
-import org.junit.ClassRule;
+import org.apereo.inspektr.audit.AuditTrailManager;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.rules.SpringClassRule;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.rules.SpringMethodRule;
 
 import java.nio.charset.StandardCharsets;
-import java.time.LocalDate;
 import java.util.Date;
 
-import static org.junit.Assert.*;
+import static junit.framework.TestCase.*;
 
 /**
  * This is {@link RestAuditTrailManagerTests}.
@@ -32,32 +37,41 @@ import static org.junit.Assert.*;
  */
 @SpringBootTest(classes = {
     RefreshAutoConfiguration.class,
+    CasSupportRestAuditConfiguration.class,
     CasCoreUtilSerializationConfiguration.class
 })
-public class RestAuditTrailManagerTests {
-    @ClassRule
-    public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
-
+@TestPropertySource(properties = {
+    "cas.audit.rest.url=http://localhost:9296",
+    "cas.audit.rest.asynchronous=false"
+})
+@Getter
+public class RestAuditTrailManagerTests extends BaseAuditConfigurationTests {
     private static final ObjectMapper MAPPER = new ObjectMapper().findAndRegisterModules();
 
     @Rule
     public final SpringMethodRule springMethodRule = new SpringMethodRule();
 
-    @Test
-    public void verifyAction() throws Exception {
-        val props = new AuditRestProperties();
-        props.setUrl("http://localhost:9296");
-        val r = new RestAuditTrailManager(props);
-        r.setAsynchronous(false);
+    @Autowired
+    @Qualifier("restAuditTrailManager")
+    private AuditTrailManager auditTrailManager;
 
+    private static String DATA;
+
+    @BeforeClass
+    public static void initialize() throws JsonProcessingException {
         val audit = new AuditActionContext("casuser", "resource", "action",
             "CAS", new Date(), "123.456.789.000", "123.456.789.000");
-        val data = MAPPER.writeValueAsString(CollectionUtils.wrapSet(audit));
+        DATA = MAPPER.writeValueAsString(CollectionUtils.wrapSet(audit));
+    }
+
+    @Test
+    @Override
+    public void verifyAuditManager() {
         try (val webServer = new MockWebServer(9296,
-            new ByteArrayResource(data.getBytes(StandardCharsets.UTF_8), "REST Output"), MediaType.APPLICATION_JSON_VALUE)) {
+            new ByteArrayResource(DATA.getBytes(StandardCharsets.UTF_8), "REST Output"), MediaType.APPLICATION_JSON_VALUE)) {
             webServer.start();
-            r.record(audit);
-            assertFalse(r.getAuditRecordsSince(LocalDate.now().minusDays(2)).isEmpty());
+            assertTrue(webServer.isRunning());
+            super.verifyAuditManager();
         } catch (final Exception e) {
             throw new AssertionError(e.getMessage(), e);
         }
